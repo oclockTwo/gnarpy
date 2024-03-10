9a13e22e0be45ad7af014330b2653ed8c70da32d
Bing Ni <nibing1220@qq.com>
2024-03-11 06:04:33 +0800

    add plausible

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter, Lora } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header/Header";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 const lora = Lora({
@@ -29,6 +30,11 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="light">
+      <Script
+        defer
+        data-domain="gnarpy.com"
+        src="https://yearbookai.online/js/script.js"
+      />
       <body className={lora.className}>
         <Header />
         {children}
